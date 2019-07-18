ccb99465a7f1a88518c7829ffa05e36cf5d2c5fd
Winn Y Cui <pyrinox@Winns-MacBook-Pro.local>
2019-07-18 12:04:06 -0700

    updated pyPI version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='crystal-ball',
-    version='0.1.4',
+    version='0.1.5',
     author='Winn Y Cui',
     author_email='winn.yc@berkeley.edu',
     packages=setuptools.find_packages(),
